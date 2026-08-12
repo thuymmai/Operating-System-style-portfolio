905f0679ca5d23e5e6764afa5d5a053eb069ca8f
thuymmai <thuymmai11@gmail.com>
2026-08-11 19:13:19 -0500

    Set up Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 const App = () => {
   return (
     <div>
-      <h1>Welcome to my website!</h1>      
+      <h1 className="text-3xl font-bold underline">Welcome to my website!</h1>      
     </div>
     
   )
