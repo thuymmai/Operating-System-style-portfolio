caf60550c18244cd133119e752b74fbcebe62272
thuymmai <thuymmai11@gmail.com>
2026-08-12 18:43:31 -0500

    Created WindowWrapper.jsx to build window-like components Changed wallpaper to old-painted-textured.jpg

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { Dock, Navbar, Welcome } from "#components";
+import gsap from "gsap";
+
+import { Draggable } from "gsap/Draggable";
+gsap.registerPlugin(Draggable);
 
 const App = () => {
   return (
---
+++ src/hoc/WindowWrapper.jsx
@@ -0,0 +1,27 @@
+import useWindowStore from "#store/window.js";
+import { useRef } from "react";
+
+const WindowWrapper = (Component, windowKey) => {
+    const Wrapped = (props) => {
+        const { focusWindow, windows } = useWindowStore();
+        const { isOpen, zIndex } = windows[windowKey];
+        const ref = useRef(null);
+
+        return ( 
+            <section id={windowKey} ref={ref} style={{zIndex}}
+                className="absolute">
+                <Component {...props}/>
+            </section>
+        );
+    };
+
+    Wrapped.displayName = `WindowWrapper(${Component.displayName || Component.name || "Component"})`;
+
+    return Wrapped;
+
+  return (
+    <div>WindowWrapper</div>
+  );
+};
+
+export default WindowWrapper;
