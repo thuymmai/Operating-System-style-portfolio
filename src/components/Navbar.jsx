d6a810b8e4e701dd2939fb81590c27802394bb2e
thuymmai <thuymmai11@gmail.com>
2026-08-14 18:40:11 -0500

    Removed logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <nav>
         <div>
-            <img src="/images/logo.svg" alt="logo"/>
+            {/* <img src="/images/logo.svg" alt="logo"/> */}
             <p className="font-bold">thuyOS</p>
 
             <ul>                
