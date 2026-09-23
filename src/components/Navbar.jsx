f9e059d99806077055186d58ed826b8171cb6160
Thuy Mai <thuymmai11@gmail.com>
2026-09-22 19:57:10 -0500

    Changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
     <nav>
       <div>
         {/* icon from lucide react */}
-        <img src="../images/ghost.svg" alt="logo" />
+        <img src="public\images\ghost.svg" alt="logo" />
         <p className="font-bold">thuyOS</p>
 
         <ul>
