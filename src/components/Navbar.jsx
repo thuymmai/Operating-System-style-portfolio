7927534cc8b85b6a1402347583266ff310b2e7b1
Thuy Mai <thuymmai11@gmail.com>
2026-09-22 19:55:46 -0500

    Changed path directory

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,41 +3,40 @@ import dayjs from "dayjs";
 import { navIcons, navLinks } from "#constants/index.js";
 import useWindowStore from "#store/window";
 
-
-
 const Navbar = () => {
-
-    {/* openWindow functionality here */}
-    const { openWindow } = useWindowStore();
+  {
+    /* openWindow functionality here */
+  }
+  const { openWindow } = useWindowStore();
 
   return (
     <nav>
-        <div>
-            {/* icon from lucide react */}
-            <img src="/images/ghost.svg" alt="logo"/>
-            <p className="font-bold">thuyOS</p>
+      <div>
+        {/* icon from lucide react */}
+        <img src="../images/ghost.svg" alt="logo" />
+        <p className="font-bold">thuyOS</p>
 
-            <ul>                
-                {/* type comes from src/constants/index.js/navLinks */}
-                {navLinks.map(({ id, name, type }) => (
-                    <li key={id} onClick={() => openWindow(type)}>
-                        <p>{name}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+        <ul>
+          {/* type comes from src/constants/index.js/navLinks */}
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-        <div>
-            <ul>
-                {navIcons.map(({ id, img }) => (
-                    <li key={id}>
-                        <img src={img} className="icon-hover" alt={`icon-${id}`} />
-                    </li>
-                ))}
-            </ul>
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
 
-            <time>{dayjs().format('ddd, MMM D, h:mm A')}</time>
-        </div>
+        <time>{dayjs().format("ddd, MMM D, h:mm A")}</time>
+      </div>
     </nav>
   );
 };
