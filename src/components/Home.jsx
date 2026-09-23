bbea1aff3ffc978e66f24d604b85b8266f93bba0
Thuy Mai <thuymmai11@gmail.com>
2026-09-22 19:12:03 -0500

    Changes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,44 +8,41 @@ import { Draggable } from "gsap/all";
 // if children does not exist, return empty array
 const projects = locations.work?.children ?? [];
 const Home = () => {
+  const { setActiveLocation } = useLocationStore();
 
-    const { setActiveLocation } = useLocationStore();
+  // click on a folder and it opens
+  const { openWindow } = useWindowStore();
 
-    // click on a folder and it opens
-    const { openWindow } = useWindowStore();
+  const handleOpenProjectFinder = (project) => {
+    setActiveLocation(project);
+    openWindow("finder");
+  };
 
-    const handleOpenProjectFinder = (project) => {
-        setActiveLocation(project);
-        openWindow("finder");
-    };
-
-    useGSAP(() => {
-        // folder class becomes draggable
-        Draggable.create(".folder");
-
-    }, []);
+  useGSAP(() => {
+    // folder class becomes draggable
+    Draggable.create(".folder");
+  }, []);
 
   return (
     <section id="home">
-        <ul>
-            {/* map each individual project */}
-            {projects.map((project) => (
-                /* use the word folder ("group folder") to target folders with GSAP so they can be draggable */
-                <li 
-                key={project.id} 
-                className={clsx("group folder", project.windowPosition)}
-                
-                // open the handleOpenProjectFinder here and pass "project"
-                onClick={() => handleOpenProjectFinder(project)}
-                >
-                    {/*/<img src="/images/folder.png" alt={project.name}/>*/}
-                    <img src={`${import.meta.env.Operating-System-style-portfolio}images/folder.png`} alt="folder"/>
+      <ul>
+        {/* map each individual project */}
+        {projects.map((project) => (
+          /* use the word folder ("group folder") to target folders with GSAP so they can be draggable */
+          <li
+            key={project.id}
+            className={clsx("group folder", project.windowPosition)}
+            // open the handleOpenProjectFinder here and pass "project"
+            onClick={() => handleOpenProjectFinder(project)}
+          >
+            <img src="/images/folder.png" alt={project.name} />
+            {/*<img src={`${import.meta.env.Operating-System-style-portfolio}images/folder.png`} alt="folder"/>*/}
 
-                    {/* render p tag that will render the project's name */}
-                    <p>{project.name}</p>
-                </li>
-            ))}
-        </ul>
+            {/* render p tag that will render the project's name */}
+            <p>{project.name}</p>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
